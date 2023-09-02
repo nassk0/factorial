17e65bd05c774c35f04a2d16e95b21f025708892
а <>
2023-09-03 02:30:52 +0500

    Факториал

--- OOP1/OOP1.cpp
+++ OOP1/OOP1.cpp
@@ -1,7 +1,20 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
 
+int factorial(int a)
+{
+	int x = a;
+	for (int i = 1; i < a; i++)
+		x = i * x;
+	return x;
+}
 int main()
 {
-    //преподаватель ыыы смотрит
+	int a;
+	cin >> a;
+	if (a == 0)
+		cout << 1;
+	else
+		cout << factorial(a);
 }
